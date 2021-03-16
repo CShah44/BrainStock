35acfa01927a869d289e2eba48221da5930359f5
CShah44 <chaityashah36@gmail.com>
2021-03-16 12:05:44 +0530

    Started with Flask

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/search')
+def search_page():
+    return render_template('search.html')
+
 
 if __name__ == '__main__':
-    pass
+    app.run(debug=True)
